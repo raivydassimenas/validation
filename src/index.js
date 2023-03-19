6b8ad8bf37af2cf6f4ec4ffb538986c60a831b74
Raivydas Šimėnas <raivydas.simenas@gmail.com>
2023-03-19 16:32:07 +0200

    Start index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import "./style.css";
+
+const submit = document.querySelector("#submit");
+
+submit.addEventListener("click", e => {
+    const email = document.querySelector("#email");
+    const country = document.querySelector("#country");
+    const zip = document.querySelector("#zip");
+    const password = document.querySelector("#password");
+    const confirmPassword = document.querySelector("#confirm-password");
+
+    if (email.checkValidity() && country.checkValidity() && zip.checkValidity() && password.checkValidity() && confirmPassword.checkValidity()) {
+        if (password.value != confirmPassword.value) {
+            alert("Password confirmation does not match");
+        }
+    }
+});
